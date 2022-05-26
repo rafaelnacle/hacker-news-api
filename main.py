4010d7b7f46f7ded614b877a62a3942f240ac284
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2022-05-26 22:33:31 +0000

    Basic main page structure for the API with title, link, points, author and number of comments.

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import requests
 from flask import Flask, render_template, request
 
+app = Flask("HackerNews")
+
+@app.route('/')
+def home():
+    recent_news_url = f"http://hn.algolia.com/api/v1/search_by_date?tags=story"
+    recent_news_json = requests.get(recent_news_url).json()
+    
+    return render_template('index.html', recent_news=recent_news_json)
+
+
+app.run(host="0.0.0.0")
+
+
