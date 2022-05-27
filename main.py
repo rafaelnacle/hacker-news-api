65acdf0a9cb63dcfb0d197a8d26e163b520cd11b
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2022-05-27 00:23:29 +0000

    Home route with order_by query filter

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask("HackerNews")
 
 @app.route('/')
-def home():
-    recent_news_url = f"http://hn.algolia.com/api/v1/search_by_date?tags=story"
-    recent_news_json = requests.get(recent_news_url).json()
+def home():     
+    if request.args.get('order_by') == "popular" or request.args.get('order_by') == "None":
+        url_popular = f"https://hn.algolia.com/api/v1/search?tags=story"
+        json_req = requests.get(url_popular).json()
+        
+        return render_template('index.html', json_req=json_req)
+    elif request.args.get('order_by') == "new":
+        url_new = f"http://hn.algolia.com/api/v1/search_by_date?tags=story"
+        json_req = requests.get(url_new).json()
+        
+        return render_template('index.html', json_req=json_req)
+    else:
+        url_home = f"https://hn.algolia.com/api/v1/search?tags=story"
+        json_req = requests.get(url_home).json()
+        
+        return render_template('index.html', json_req=json_req)
     
-    return render_template('index.html', recent_news=recent_news_json)
+
 
 
 app.run(host="0.0.0.0")
